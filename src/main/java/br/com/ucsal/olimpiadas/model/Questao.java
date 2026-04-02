494b922e25a58bfe1bdd5caa7d7d57947f4018af
IcaroDevMS <icaromatossilva@gmail.com>
2026-04-02 00:41:17 -0300

    refactor: aplicar Interface Segregation Principle (ISP) separando Exibivel de Questao

--- src/main/java/br/com/ucsal/olimpiadas/model/Questao.java
+++ src/main/java/br/com/ucsal/olimpiadas/model/Questao.java
@@ -2,11 +2,10 @@ package br.com.ucsal.olimpiadas.model;
 
 import java.util.Scanner;
 
-public interface Questao {
+public interface Questao extends Exibivel {
     long getId();
     void setId(long id);
     long getProvaId();
     void setProvaId(long provaId);
-    void exibir();
     boolean verificarResposta(String resposta);
 }
